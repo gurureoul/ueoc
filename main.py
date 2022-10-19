f1f35accd4d2c70d22c4f7458c6c99ebe3a36017
Guru Reoul <gurureoul@gmail.com>
2022-10-18 23:36:16 -0400

    Got the name working correct. Added back app.run(debug=True) for development

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from ueocdata import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    # 'app' gets imported by the wsgi, so having this in main
+    # shouldn't run on the production server
+    app.run(debug=True)
