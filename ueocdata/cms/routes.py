8fbf0c855f9da002b19e7cc7b48e55ea26f63413
Guru Reoul <gurureoul@gmail.com>
2022-10-22 08:49:52 -0400

    flushed out the CMS section with a Lex Bot feature

--- ueocdata/cms/routes.py
+++ ueocdata/cms/routes.py
@@ -5,3 +5,7 @@ cms = Blueprint('cms', __name__, url_prefix='/cms')
 @cms.route('/')
 def index():
     return render_template('cms/index.html')
+
+@cms.route('/lexi')
+def lexi():
+    return render_template('cms/lexi.html')
